7ccb845ea65a0247ce49e0f612f9ddae46b85ff7
Atticus <atticus.beard@icloud.com>
2025-09-25 09:30:22 -0600

    Added image to canvas at center of screen. #TODO Change this later

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password manager.")
+
+logo = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=0, row=0, padx=20, pady=20)
+
+window.mainloop()
