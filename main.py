2f023b684f1108e5b0ba54c49144a370bc688656
Atticus <atticus.beard@icloud.com>
2025-09-26 08:27:50 -0600

    Added password generator

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
     global email_str
 
-    if len(website_entry.get()) != 0 and len(password_entry.get()) != 0 :
+    if len(website_entry.get()) != 0 and len(password_entry.get()) != 0 and len(username_entry.get()):
         is_ok = messagebox.askokcancel(title=f"{website_entry.get()}", message=f"These are the details entered: \nEmail: {username_entry.get()}\nPassword: {password_entry.get()}\nIs it ok to save?")
 
         if is_ok:
@@ -66,7 +91,7 @@ password_label.grid(column=0, row=3, sticky="e")
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3, sticky="w", pady=5)
 
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3, sticky="w", pady=5, padx=(5, 0))
 
 add_button = Button(text="Add", width=36, command=save)
